36438cfe9fda354097c717657c417779e3096cfe
niraj <rwtniraj@gmail.com>
2024-08-24 05:48:32 +0100

    update

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ connectDB();
 // esmodule fix
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+console.log('Serving static files from:', path.resolve(__dirname, '../../build'));
 
 // create engine
 const app = express();
@@ -25,6 +26,8 @@ const app = express();
 app.use(cors());
 app.use(express.json())
 app.use(morgan('dev'))
+
+const rootDirectory = path.resolve(__dirname, '../../');
 app.use(express.static(path.join(__dirname, '../../build')))
 // routes
 app.use('/api/v1/auth', authRoutes);
@@ -33,7 +36,7 @@ app.use('/api/v1/product', productRoutes)
 
 // create rest apis
 app.get('*', function(req,res){
-    res.sendFile(path.resolve(__dirname, '../../build', 'index.html'))
+    res.sendFile(path.join(rootDirectory, 'build', 'index.html'))
 })
 
 // create a port for a node server
