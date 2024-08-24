925ea09e78e7ff4b1909a9f7ab2f14ef962e4505
niraj <rwtniraj@gmail.com>
2024-08-24 05:32:16 +0100

    updated server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const app = express();
 app.use(cors());
 app.use(express.json())
 app.use(morgan('dev'))
-app.use(express.static(path.join(__dirname, '../build')))
+app.use(express.static(path.join(__dirname, '../../build')))
 // routes
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/category', categoryRoutes);
@@ -33,7 +33,7 @@ app.use('/api/v1/product', productRoutes)
 
 // create rest apis
 app.get('*', function(req,res){
-    res.sendFile(path.join(__dirname, '../build', 'index.html'))
+    res.sendFile(path.join(__dirname, '../../build', 'index.html'))
 })
 
 // create a port for a node server
